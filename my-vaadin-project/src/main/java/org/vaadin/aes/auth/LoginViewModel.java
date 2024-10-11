2c8888a9a871af9c520a5b4fe663d96f7310bf9e
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-10-11 22:21:12 +0300

    auth package is added

---
+++ my-vaadin-project/src/main/java/org/vaadin/aes/auth/LoginViewModel.java
@@ -0,0 +1,33 @@
+package org.vaadin.aes.auth;
+
+import com.vaadin.flow.component.HtmlContainer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
+import org.vaadin.aes.model.concrete.User;
+import org.vaadin.aes.model.concrete.UserCredential;
+import org.vaadin.aes.service.user.AuthenticationService;
+
+import java.util.logging.Logger;
+
+public class LoginViewModel {
+    private static final Logger log = Logger.getLogger(LoginViewModel.class.getName());
+
+    private AuthenticationService authService;
+    private User user;
+    private HtmlContainer userFeedBack;
+
+    public LoginViewModel(AuthenticationService authService, HtmlContainer userFeedBack) {
+        this.authService = authService;
+        this.userFeedBack = userFeedBack;
+    }
+
+    public User login(UserCredential creds) {
+        user = authService.login(creds);
+        if (user == null) {
+            userFeedBack.setText("Username or password is invalid");
+            log.info("User is not found. and userFeedback is updated");
+        }
+        return user;
+
+    }
+}
---
+++ my-vaadin-project/src/main/java/org/vaadin/aes/auth/SignupViewModel.java
@@ -0,0 +1,34 @@
+package org.vaadin.aes.auth;
+
+import org.vaadin.aes.model.concrete.User;
+import org.vaadin.aes.service.user.AuthenticationService;
+
+import java.util.logging.Logger;
+
+public class SignupViewModel {
+    private static final Logger log = Logger.getLogger(SignupViewModel.class.getName());
+    private AuthenticationService authService;
+    private User user;
+
+    public SignupViewModel(AuthenticationService authService) {
+        this.authService = authService;
+        this.user = new User();
+    }
+
+    public boolean signUp(User user) {
+        // İşlem sonucuna göre geri döndür
+        log.info("User is retrieved to save:");
+        return authService.signUp(user) != null;
+    }
+
+//    public void setUserData(String firstName, String lastName, String email, String phone, String username, String password) {
+//        user.setFirstName(firstName);
+//        user.setLastName(lastName);
+//        user.setEmail(email);
+//        user.setPhoneNo(phone);
+//        user.setUsername(username);
+//        user.setPassword(password);
+//    }
+
+
+}
